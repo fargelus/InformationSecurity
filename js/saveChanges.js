199dccc3296b675fa8e155281a94063098034391
fargelus <shmurnov@list.ru>
2016-11-12 11:49:47 +0300

    add button instead browser close event

--- js/saveChanges.js
+++ js/saveChanges.js
@@ -1,17 +1,19 @@
-$(window).bind("beforeunload", function(){
-	var emptyRequest = {};
-    $.ajax({
-      type: "POST",
-      data: emptyRequest,
-      url: "../cgi-bin/dump.py",
-      datatype: "json",
-      traditional: true,
-      success: function(response){
-      	window.open('', '_self', ''); 
-        window.close();
-      },
-      error: function(response){
-      	alert("Что-то пошло не так");
-      }
+$(document).ready(function() {
+  $("#dump_btn").click(function() {
+	   var emptyRequest = {};
+      $.ajax({
+        type: "POST",
+        data: emptyRequest,
+        url: "../cgi-bin/dump.py",
+        datatype: "json",
+        traditional: true,
+        success: function(response){
+        	window.open('', '_self', ''); 
+          window.close();
+        },
+        error: function(response){
+        	alert("Что-то пошло не так");
+        }
+      });
   });
 });
